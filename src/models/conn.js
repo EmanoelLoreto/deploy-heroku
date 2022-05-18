849270c147e415b8c80823f3ac24af0ffeea038f
Emanoel Loreto <emanoel.loreto@meta.com.br>
2022-05-17 21:38:42 -0300

    all

--- src/models/conn.js
+++ src/models/conn.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const URI = 'mongodb+srv://dbaula:deploy@deploytest.c6uqu.mongodb.net/?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://deploy-heroku:iIt8Er4eaP7X0WLl@deploy-heroku.v3goh.mongodb.net/?retryWrites=true&w=majority';
 
 let db = null;
 let conn = null;
@@ -12,7 +12,7 @@ const connection = async () => {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         });
-        db = conn.db('DesenvSistI');
+        db = conn.db('deploy-heroku');
         return db;
     } catch (error) {
         console.log(error.message);
