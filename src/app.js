d5163a4a8e982bf05a8571a6a70a69bab8bba22c
Emanoel Loreto <emanoel.loreto@meta.com.br>
2022-05-17 21:53:33 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -4,9 +4,13 @@ const routes = require("./routes");
 const appError = require('./errors/appError');
 const uploadConfig = require('./config/multer');
 
+const cors = require('cors')
+
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/images', express.static(uploadConfig.directory));
 
